ea0a9ccb717be27a8e43e3bceb03e5f346b7a090
felipe.fernandes@payco.com.br <feliperenanqwerty@gmail.com>
2025-03-02 20:01:13 -0300

    feat: adding user domain

---
+++ internal/user/repository/user_repository.go
@@ -0,0 +1,35 @@
+package repository
+
+import (
+	"1mao/internal/user/domain"
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+
+type UserRepository interface{
+	Create(user *domain.User) error
+	FindByEmail(email string) (*domain.User, error)
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository{
+	return &userRepository{db: db}
+}
+
+func (r *userRepository) Create(user *domain.User) error{
+	return r.db.Create(user).Error
+}
+
+func (r *userRepository) FindByEmail(email string) (*domain.User, error){
+	var user domain.User
+	if err := r.db.Where("email = ?", email).First(&user).Error; err != nil{
+		return nil, errors.New("usuario nao encontrado")
+	}
+	return &user, nil
+}
+
