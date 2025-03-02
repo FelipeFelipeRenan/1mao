0d81b4dfb81f1f77240937cd94563b9cbf04a114
felipe.fernandes@payco.com.br <feliperenanqwerty@gmail.com>
2025-03-02 19:48:23 -0300

    feat: adding user domain

---
+++ internal/user/domain/user.go
@@ -0,0 +1,20 @@
+package domain
+
+import(
+	"gorm.io/gorm"
+)
+
+type Role string
+
+const (
+	RoleClient Role = "client"
+	RoleProfessional Role = "professional"
+)
+
+type User struct {
+	gorm.Model
+	Name string `gorm:"not null"`
+	Email string `gorm:"unique;not null"`
+	Password string `gorm:"not null"`
+	Role Role `gorm:"type:varchar(20);not null"`
+}
