b2c36286d96787f09321950007c0576da0bc9b5b
felipe.fernandes@payco.com.br <feliperenanqwerty@gmail.com>
2025-03-03 17:18:48 -0300

    feat: adding user auth service

--- go.mod
+++ go.mod
@@ -2,9 +2,12 @@ module 1mao
 
 go 1.23.4
 
+require gorm.io/gorm v1.25.12
+
 require (
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/crypto v0.35.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
-	gorm.io/gorm v1.25.12
 )
---
+++ internal/user/service/auth_service.go
@@ -0,0 +1,68 @@
+package service
+
+import (
+	"1mao/internal/user/domain"
+	"1mao/internal/user/repository"
+	"errors"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+
+type AuthService interface {
+	Register(user *domain.User) error
+	Login(email, password string) (string, error)
+	
+}
+
+type authService struct {
+	userRepo repository.UserRepository
+}
+
+func NewAuthService(userRepo repository.UserRepository) AuthService{
+	return &authService{userRepo: userRepo}
+}
+
+func (s *authService) Register(user *domain.User) error{
+	// Hash da senha do usuario
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	user.Password = string(hashedPassword)
+
+	// Salvar no banco
+	return s.userRepo.Create(user)
+}
+
+func (s *authService) Login(email, password string) (string, error){
+	user, err := s.userRepo.FindByEmail(email)
+	if err != nil {
+		return "", errors.New("Usuário ou senha inválidos")
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil{
+		return "", errors.New("Usuário ou senhas invalidos")
+	}
+
+	// criar token jwt
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": user.ID,
+		"role": user.Role,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	secretKey := os.Getenv("JWT_SECRET")
+	tokenString, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+
+}
