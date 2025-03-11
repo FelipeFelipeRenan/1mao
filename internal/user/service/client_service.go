bcd787f19820f9f72743ee796f7f1ce6e0010a20
felipe.fernandes@payco.com.br <feliperenanqwerty@gmail.com>
2025-03-11 19:27:46 -0300

    test: Adding register and find by email tests

--- internal/user/service/client_service.go
+++ internal/user/service/client_service.go
@@ -14,6 +14,7 @@ import (
 
 type AuthService interface {
 	Register(user *domain.User) error
+	FindByEmail(email string)(*auth.User, error)
 	GetUserByID(userID uint) (*domain.User, error)
 	Login(email, password string) (string, error)
 	GetAllUsers() ([]domain.User, error)
---
+++ internal/user/service/client_service_test.go
@@ -0,0 +1,56 @@
+package service
+
+import (
+	"1mao/internal/user/domain"
+	"1mao/internal/user/repository"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+
+func hashPassword(password string) string{
+	hashed, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hashed)
+}
+
+func TestRegister_Sucess(t *testing.T){
+	mockRepo := new(repository.MockUserRepository)
+	authService := NewAuthService(mockRepo)
+
+	user := &domain.User{
+		Email: "user@email.com",
+		Password: "senha123",
+		Role: "client",
+	}
+
+	mockRepo.On("Create", mock.AnythingOfType("*domain.User")).Return(nil)
+
+	err := authService.Register(user)
+	assert.NoError(t, err)
+	mockRepo.AssertExpectations(t)
+
+}
+
+func TestFindByEmail_Success(t *testing.T){
+	mockRepo := new(repository.MockUserRepository)
+	authService := NewAuthService(mockRepo)
+
+	expectedUser := &domain.User{
+		Model: gorm.Model{ID: 1},
+		Email: "user@email.com",
+		Password: hashPassword("senha123"),
+		Role: domain.RoleClient,
+	}
+
+	mockRepo.On("FindByEmail", "user@email.com").Return(expectedUser,nil)
+	
+	user, err := authService.FindByEmail("user@email.com")
+	assert.NoError(t, err)
+
+	assert.Equal(t, expectedUser.Email, user.Email)
+	mockRepo.AssertExpectations(t)
+}
