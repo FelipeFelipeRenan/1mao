aea1188193812af690f9c56131de11879fecab90
felipe.fernandes@payco.com.br <feliperenanqwerty@gmail.com>
2025-03-17 19:23:18 -0300

    feat: Adding professional service

---
+++ internal/professional/service/professional_service.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"1mao/internal/professional/domain"
+	"1mao/internal/professional/repository"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type ProfessionalService interface {
+	Register(professional *domain.Professional) error
+	GetProfessionalByID(id uint) (*domain.Professional, error)
+	GetAllProfessionals() ([]domain.Professional, error)
+}
+
+type professionalService struct {
+	repo repository.ProfessionalRepository
+}
+
+func NewProfessionalService(repo *repository.ProfessionalRepository) ProfessionalService {
+	return &professionalService{repo: *repo}
+}
+
+func (s *professionalService) Register(professional *domain.Professional) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(professional.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	professional.Password = string(hashedPassword)
+	return s.repo.Create(professional)
+}
+
+
+func (s *professionalService) GetProfessionalByID(id uint)(*domain.Professional, error){
+	return s.repo.FindByID(id)
+}
+
+func (s *professionalService) GetAllProfessionals()([]domain.Professional, error){
+	return s.repo.GetAllProfessionals()
+}
