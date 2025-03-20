fa8a31cd77da8bb3484bd2f73124e048d8b4b169
felipe.fernandes@payco.com.br <feliperenanqwerty@gmail.com>
2025-03-20 18:41:07 -0300

    feat: adding notification handler using websocket

---
+++ internal/notification/websocket/notification_handler.go
@@ -0,0 +1,31 @@
+package websocket
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func HandleConnections(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Erro ao fazer upgrade para WebSocket:", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		messageType, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Erro ao ler mensagem:", err)
+			break
+		}
+		fmt.Printf("Mensagem recebida: %s\n", msg)
+		conn.WriteMessage(messageType, []byte("Mensagem recebida!"))
+	}
+}
