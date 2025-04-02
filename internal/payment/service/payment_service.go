47733bfdb4aa8c832462ea9dbc7c4716fc8beac1
felipe.fernandes@payco.com.br <feliperenanqwerty@gmail.com>
2025-04-02 19:25:34 -0300

    feat: adding method to create a payment

--- internal/payment/service/payment_service.go
+++ internal/payment/service/payment_service.go
@@ -1,15 +1,42 @@
 package service
 
-import "1mao/internal/payment/repository"
+import (
+	"1mao/internal/payment/domain"
+	"1mao/internal/payment/repository"
+)
 
 type PaymentService interface {
+	CreatePayment(bookingID string, amount int64, method string) (*domain.Transaction, error)
 }
 type paymentService struct {
 	repo repository.PaymentRepository
+	stripe *StripeClient
 }
 
-func NewPaymentService(repo repository.PaymentRepository) PaymentService {
+func NewPaymentService(repo repository.PaymentRepository, stripeKey string) PaymentService {
 	return &paymentService{
 		repo: repo,
+		stripe: NewStripeClient(stripeKey),
 	}
+}
+
+
+func (s *paymentService) CreatePayment(bookingID string, amount int64, method string) (*domain.Transaction, error){
+	// criar intent no stripe
+	intent, err := s.stripe.CreatePaymentIntent(amount, "brl")
+	if err != nil {
+		return nil, err
+	}
+
+	transaction := domain.Transaction{
+		BookingID: bookingID,
+		Amount: amount,
+		Currency: "BRL",
+		Status: "pending",
+		PaymentMethod: method,
+		GatewayID: intent.ID,
+	}
+
+	err = s.repo.CreateTransaction(transaction)
+	return &transaction, err
 }
